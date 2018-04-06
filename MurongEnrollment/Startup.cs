1835c0fee57dd71993107a0853ce1c4627cb8a7c
Mark Cacal <Kram@DESKTOP-PJQQAR8>
2018-04-06 20:45:59 +0800

    Add project files.

---
+++ MurongEnrollment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MurongEnrollment.Startup))]
+namespace MurongEnrollment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
